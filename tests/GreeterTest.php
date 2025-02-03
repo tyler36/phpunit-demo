9ea55df8ba9e4515e96273c04610b97616628a32
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-02-03 16:23:40 +0900

    deliberately bad test for debugging

--- tests/GreeterTest.php
+++ tests/GreeterTest.php
@@ -10,7 +10,7 @@ class GreeterTest extends TestCase
 
         $greeting = $greeter->greet('Alice');
 
-        $this->assertSame('Hello, Alice!', $greeting);
+        $this->assertSame('Hello, David!', $greeting);
     }
 
     public function testException(): void
